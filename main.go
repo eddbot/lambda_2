220edb9ed2dc7bad3bc3ac67c5bdf89ce36307ec
eddbot <edwardsansome@gmail.com>
2023-02-23 00:44:33 +0000

    add comment

--- main.go
+++ main.go
@@ -22,6 +22,10 @@ type Output struct {
 
 func handler(ctx context.Context, i Input) (Output, error) {
 
+	// this could be updated to use a dynamic timestamp path
+	// rather than the seconds directly
+	// https://docs.aws.amazon.com/step-functions/latest/dg/amazon-states-language-wait-state.html
+
 	future := time.Now().Add(time.Hour * (180 * 24))
 	now := time.Now()
 	timeToWaitInSeconds := int(future.Sub(now).Seconds())
